62ebf1657008084ae16fac8c790b6b64b25588a2
PriyankaSingla013 <102787489+PriyankaSingla013@users.noreply.github.com>
2024-11-05 19:54:31 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (98.79%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isBalanced(self, num: str) -> bool:
+        sum=0
+        for i in range (len(num)):
+            if(i%2==0):
+                sum+=int(num[i])
+            else:
+                sum-=int(num[i])
+        return sum==0
