d058ea49247475f178d4c9514b3cd7ac8b496527
PriyankaSingla013 <102787489+PriyankaSingla013@users.noreply.github.com>
2025-01-02 13:02:59 +0000

    Time: 38 ms (55.62%) | Memory: 17.5 MB (21.06%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,12 @@
 class Solution:
     def fib(self, n: int) -> int:
+        dict={}
         def fibo(n):
             if n<2:
                 return n
-            return fibo(n-1)+fibo(n-2)
+            if(n in dict.keys()):
+                return dict[n]
+            dict[n]= fibo(n-1)+fibo(n-2)
+            return dict[n]
         return fibo(n)
         
