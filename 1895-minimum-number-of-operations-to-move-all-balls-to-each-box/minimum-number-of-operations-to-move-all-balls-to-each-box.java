f74fa2e5a31a8b1ba3fd9f78699560a64b8eab19
PriyankaSingla013 <102787489+PriyankaSingla013@users.noreply.github.com>
2025-01-06 15:55:55 +0000

    Time: 121 ms (36.81%) | Memory: 45.3 MB (11.46%) - LeetSync

---
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n=boxes.length();
+        int[] res=new int[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(boxes.charAt(j)=='1')
+                res[i]+=Math.abs(i-j);
+            }
+        }
+        return res;
+    }
+}
